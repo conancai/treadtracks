c7885fad99e810c49fd97ab4a0a1b88363a722b1
Ian Birnam <ian3000@gmail.com>
2014-04-03 13:31:43 -0700

    first iteration of stats page

--- gen/com/example/treadtracksproto/R.java
+++ gen/com/example/treadtracksproto/R.java
@@ -24,7 +24,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f080006;
+        public static final int stat_title=0x7f080001;
+        public static final int this_week=0x7f080003;
+        public static final int this_week_div=0x7f080004;
+        public static final int this_week_div2=0x7f080005;
+        public static final int this_week_title_div=0x7f080002;
+        public static final int title_div=0x7f080000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -38,6 +44,8 @@ public final class R {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050002;
+        public static final int stats_title=0x7f050004;
+        public static final int this_week=0x7f050005;
         public static final int title_activity_stats_page=0x7f050003;
     }
     public static final class style {
