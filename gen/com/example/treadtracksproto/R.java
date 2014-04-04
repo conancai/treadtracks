ab3a595ae6414a4342ef94b989a0c744313d9f1e
Zhiyuan Xu <k.zhiyuan.xu@gmail.com>
2014-04-04 13:18:02 -0700

    initial player activity page

--- gen/com/example/treadtracksproto/R.java
+++ gen/com/example/treadtracksproto/R.java
@@ -21,21 +21,40 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int back=0x7f020000;
+        public static final int ff=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int icon_22164=0x7f020003;
+        public static final int icon_22165=0x7f020004;
+        public static final int icon_22173=0x7f020005;
+        public static final int icon_22174=0x7f020006;
+        public static final int icon_5029=0x7f020007;
+        public static final int rounded_button=0x7f020008;
     }
     public static final class id {
-        public static final int action_settings=0x7f080022;
-        public static final int bottom_last_week_line=0x7f08001d;
+        public static final int BPM=0x7f080019;
+        public static final int action_settings=0x7f08002d;
+        public static final int album_art=0x7f080014;
+        public static final int bottom_last_week_line=0x7f080028;
+        public static final int bpm_down=0x7f08001c;
+        public static final int bpm_num=0x7f080018;
+        public static final int bpm_up=0x7f08001b;
         public static final int button1=0x7f080008;
         public static final int editText1=0x7f080011;
-        public static final int last_pace_time=0x7f080021;
-        public static final int last_week=0x7f08001c;
-        public static final int last_week_div=0x7f08001e;
-        public static final int last_week_div2=0x7f080020;
-        public static final int last_week_title_div=0x7f08001a;
-        public static final int last_week_top=0x7f08001f;
-        public static final int pace_time=0x7f080019;
-        public static final int stat_title=0x7f080013;
+        public static final int imageView1=0x7f080017;
+        public static final int last_pace_time=0x7f08002c;
+        public static final int last_week=0x7f080027;
+        public static final int last_week_div=0x7f080029;
+        public static final int last_week_div2=0x7f08002b;
+        public static final int last_week_title_div=0x7f080025;
+        public static final int last_week_top=0x7f08002a;
+        public static final int next_song=0x7f080015;
+        public static final int pace_time=0x7f080024;
+        public static final int play=0x7f080012;
+        public static final int previous_song=0x7f080016;
+        public static final int seekBar1=0x7f080013;
+        public static final int start_run_button=0x7f08001a;
+        public static final int stat_title=0x7f08001e;
         public static final int textView1=0x7f080000;
         public static final int textView10=0x7f080009;
         public static final int textView11=0x7f08000b;
@@ -52,13 +71,13 @@ public final class R {
         public static final int textView7=0x7f080006;
         public static final int textView8=0x7f080007;
         public static final int textView9=0x7f08000a;
-        public static final int this_week=0x7f080015;
-        public static final int this_week_div=0x7f080016;
-        public static final int this_week_div2=0x7f080018;
-        public static final int this_week_title_div=0x7f080014;
-        public static final int this_week_top=0x7f080017;
-        public static final int title_div=0x7f080012;
-        public static final int top_last_week_line=0x7f08001b;
+        public static final int this_week=0x7f080020;
+        public static final int this_week_div=0x7f080021;
+        public static final int this_week_div2=0x7f080023;
+        public static final int this_week_title_div=0x7f08001f;
+        public static final int this_week_top=0x7f080022;
+        public static final int title_div=0x7f08001d;
+        public static final int top_last_week_line=0x7f080026;
     }
     public static final class layout {
         public static final int activity_calibration_page=0x7f030000;
