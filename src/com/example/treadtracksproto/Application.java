9f78b54c803d61f8f6f0c14e372d719df43d3a7e
Ian Birnam <ian3000@gmail.com>
2014-04-16 22:11:54 -0700

    parse stuff implemented, but adaptor or factory is broken

--- src/com/example/treadtracksproto/Application.java
+++ src/com/example/treadtracksproto/Application.java
@@ -11,10 +11,23 @@ public class Application extends android.app.Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
+		ParseObject.registerSubclass(StatsPost.class);
 		Parse.initialize(this, "tD1btrqGO7hWJrjE0ThswtnPYXKrvohWQSpoktHI",
 				"U8tZwhqiwY2NOsGLkOdrWZFytRMouW6Eqi2sQVtq");
 		ParseObject testObject = new ParseObject("TestObject");
 		testObject.put("foo", "bar");
+		StatsPost post1 = new StatsPost();
+		post1.setDate("2-4-14");
+		post1.setDistance("5");
+		post1.setPace("10:05 Mi.");
+		post1.setTime("30 Minutes");
+		StatsPost post2 = new StatsPost();
+		post2.setDate("2-5-14");
+		post2.setDistance("6");
+		post2.setPace("14:02 Mi.");
+		post2.setTime("53 Minutes");
+		post1.saveInBackground();
+		post2.saveInBackground();
 		testObject.saveInBackground();
 	}
 
--- src/com/example/treadtracksproto/StatsPage.java
+++ src/com/example/treadtracksproto/StatsPage.java
@@ -1,43 +1,157 @@
 package com.example.treadtracksproto;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.Menu;
 import android.view.View;
-import android.widget.Button;
+import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
-import com.parse.Parse;
-import com.parse.ParseAnalytics;
+
+import com.parse.ParseACL;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseQueryAdapter;
+import com.parse.SaveCallback;
 
 public class StatsPage extends Activity {
 
 	private String estimatedDistance;
 	private EditText distBox;
 	private TextView finalText;
+	private ParseQueryAdapter.QueryFactory<StatsPost> factory;
+	private ParseQueryAdapter<StatsPost> posts;
+
+	private void doListQuery() {
+		posts.loadObjects();
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_stats_page);
-		finalText = (TextView) findViewById(R.id.lastDistanceFinal);
-		distBox = (EditText) findViewById(R.id.lastDistanceEdit);
-		estimatedDistance = distBox.getText().toString();
+
+		factory = new ParseQueryAdapter.QueryFactory<StatsPost>() {
+			public ParseQuery<StatsPost> create() {
+				ParseQuery<StatsPost> query = StatsPost.getQuery();
+				query.orderByDescending("createdAt");
+				query.setLimit(5);
+				return query;
+			}
+		};
+
+		posts = new ParseQueryAdapter<StatsPost>(this, factory) {
+			@Override
+			public View getItemView(StatsPost post, View view, ViewGroup parent) {
+				if (view == null) {
+					view = View
+							.inflate(getContext(), R.layout.stats_item, null);
+				}
+				super.getItemView(post, view, parent);
+				TextView dateView = (TextView) view.findViewById(R.id.dateView);
+				TextView distanceView = (TextView) view
+						.findViewById(R.id.distanceView);
+				TextView paceView = (TextView) view.findViewById(R.id.paceView);
+				TextView timeView = (TextView) view.findViewById(R.id.timeView);
+				String date = post.getDate();
+				String dist = post.getDistance();
+				String pace = post.getPace();
+				String time = post.getTime();
+				dateView.setText(post.getDate());
+				distanceView.setText(post.getDistance());
+				paceView.setText(post.getPace());
+				timeView.setText(post.getTime());
+				return view;
+			}
+		};
+
+		posts.setAutoload(false);
+
+		ListView postsView = (ListView) this.findViewById(R.id.stats_view);
+		postsView.setAdapter(posts);
+
+		AlertDialog.Builder alert = new AlertDialog.Builder(StatsPage.this);
+		alert.setTitle("Enter Distance");
+		final EditText input = new EditText(StatsPage.this);
+		input.setInputType(InputType.TYPE_CLASS_TEXT
+				| InputType.TYPE_TEXT_FLAG_CAP_SENTENCES);
+		alert.setView(input);
+		// Handle the dialog input
+		alert.setPositiveButton("Done", new DialogInterface.OnClickListener() {
+			public void onClick(DialogInterface dialog, int which) {
+				// Create a post.
+				StatsPost post = new StatsPost();
+				post.setDate("2-4-14");
+				post.setDistance(input.getText().toString());
+				post.setPace("10:05 Mi.");
+				post.setTime("30 Minutes");
+				ParseACL acl = new ParseACL();
+				// Give public read access
+				acl.setPublicReadAccess(true);
+				post.setACL(acl);
+				// Save the post
+				post.saveInBackground(new SaveCallback() {
+					@Override
+					public void done(ParseException e) {
+						// Update the list view
+						doListQuery();
+					}
+				});
+			}
+		});
+		alert.setNegativeButton("Cancel",
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int which) {
+						// Do nothing.
+					}
+				});
+		alert.create().show();
+
+		// finalText = (TextView) findViewById(R.id.lastDistanceFinal);
+		// distBox = (EditText) findViewById(R.id.lastDistanceEdit);
+		// estimatedDistance = distBox.getText().toString();
 	}
 
-	public void confirm(View v) {
-		estimatedDistance = distBox.getText().toString();
-		Button confirm = (Button) findViewById(R.id.confirmButton);
-		Button clear = (Button) findViewById(R.id.clearButton);
-		confirm.setVisibility(View.GONE);
-		clear.setVisibility(View.GONE);
-		distBox.setVisibility(View.GONE);
-		finalText.setText(estimatedDistance);
-		finalText.setVisibility(View.VISIBLE);
+	// public void confirm(View v) {
+	// estimatedDistance = distBox.getText().toString();
+	// Button confirm = (Button) findViewById(R.id.confirmButton);
+	// Button clear = (Button) findViewById(R.id.clearButton);
+	// confirm.setVisibility(View.GONE);
+	// clear.setVisibility(View.GONE);
+	// distBox.setVisibility(View.GONE);
+	// finalText.setText(estimatedDistance);
+	// finalText.setVisibility(View.VISIBLE);
+	// }
+	//
+	// public void reset(View v) {
+	// distBox.setText(estimatedDistance, TextView.BufferType.EDITABLE);
+	// }
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		doListQuery();
 	}
 
-	public void reset(View v) {
-		distBox.setText(estimatedDistance, TextView.BufferType.EDITABLE);
+	/*
+	 * Called when the Activity is no longer visible at all. Stop updates and
+	 * disconnect.
+	 */
+	@Override
+	public void onStop() {
+		super.onStop();
+	}
+
+	/*
+	 * Called when the Activity is restarted, even before it becomes visible.
+	 */
+	@Override
+	public void onStart() {
+		super.onStart();
 	}
 
 	@Override
