b1ef04e7df9389d6355faff797c8131e88ffd962
Ian Birnam <ian3000@gmail.com>
2014-05-03 16:15:12 -0700

    added custom bar to kats playlsit pages

--- src/com/example/treadtracksproto/PlaylistActivity.java
+++ src/com/example/treadtracksproto/PlaylistActivity.java
@@ -3,92 +3,98 @@ package com.example.treadtracksproto;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import android.app.Activity;
 import android.app.ActionBar;
 import android.app.Fragment;
 import android.app.ListActivity;
 import android.content.ContentResolver;
-import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.os.Build;
-import android.provider.MediaStore;
 
 public class PlaylistActivity extends ListActivity {
 
 	private TextView playlistName;
 	private HashMap<String, String> playlists;
-	
+
 	private ArrayList<String> playlistN;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_playlist);
-		
+
+		getActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+		getActionBar().setCustomView(R.layout.actionbar);
+
 		// http://www.howtosolvenow.com/2013/02/how-to-create-a-playlist/
 		ContentResolver cr = this.getContentResolver();
-	    Uri uri= MediaStore.Audio.Playlists.EXTERNAL_CONTENT_URI;
-	    String[] projection = { MediaStore.Audio.Playlists._ID, MediaStore.Audio.Playlists.NAME };
-	    Cursor cursor = cr.query(uri, projection, null, null, null);
-	    cursor.moveToFirst();
-	    
-	    playlists = new HashMap<String, String>();
-	    playlistN = new ArrayList<String>();
-	    
-	    String id = "all";
-	    String name = "All Songs"; 
-	    playlists.put(name,id);
-	    playlistN.add(name);
-	    
-	    for (int i = 0; i < cursor.getCount(); i++) {
-	    	id = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Playlists._ID));
-	    	name = cursor.getString(cursor.getColumnIndex(MediaStore.Audio.Playlists.NAME));
-	    	//PlaylistItem item = new PlaylistItem(id, name);
-	    	playlists.put(name, id);
-	    	playlistN.add(name);
-	    	cursor.moveToNext();
-	    }
-	    
-	    playlistName = (TextView) findViewById(R.id.playlist_name);
-	    
-	    ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.playlist_row_item, playlistN);
-	    //PlaylistAdapter adapter = new PlaylistAdapter(this, R.layout.activity_playlist, playlists);
-	    
-	    setListAdapter(adapter);
+		Uri uri = MediaStore.Audio.Playlists.EXTERNAL_CONTENT_URI;
+		String[] projection = { MediaStore.Audio.Playlists._ID,
+				MediaStore.Audio.Playlists.NAME };
+		Cursor cursor = cr.query(uri, projection, null, null, null);
+		cursor.moveToFirst();
+
+		playlists = new HashMap<String, String>();
+		playlistN = new ArrayList<String>();
+
+		String id = "all";
+		String name = "All Songs";
+		playlists.put(name, id);
+		playlistN.add(name);
+
+		for (int i = 0; i < cursor.getCount(); i++) {
+			id = cursor.getString(cursor
+					.getColumnIndex(MediaStore.Audio.Playlists._ID));
+			name = cursor.getString(cursor
+					.getColumnIndex(MediaStore.Audio.Playlists.NAME));
+			// PlaylistItem item = new PlaylistItem(id, name);
+			playlists.put(name, id);
+			playlistN.add(name);
+			cursor.moveToNext();
+		}
+
+		playlistName = (TextView) findViewById(R.id.playlist_name);
+
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				R.layout.playlist_row_item, playlistN);
+		// PlaylistAdapter adapter = new PlaylistAdapter(this,
+		// R.layout.activity_playlist, playlists);
+
+		setListAdapter(adapter);
 
 		/*
-		if (savedInstanceState == null) {
-			getFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}*/
+		 * if (savedInstanceState == null) {
+		 * getFragmentManager().beginTransaction() .add(R.id.container, new
+		 * PlaceholderFragment()).commit(); }
+		 */
 	}
-	
-	protected void onListItemClick(ListView listView, View view, int position, long id) {
+
+	protected void onListItemClick(ListView listView, View view, int position,
+			long id) {
 		super.onListItemClick(listView, view, position, id);
-		//PlaylistItem item = (PlaylistItem) listView.getItemAtPosition(position);
-		
+		// PlaylistItem item = (PlaylistItem)
+		// listView.getItemAtPosition(position);
+
 		String name = (String) getListAdapter().getItem(position);
 		String playlistID = playlists.get(name);
-		
+
 		Log.d("Debug", "name: " + name + " playlistID: " + playlistID);
-		
+
 		Intent i = new Intent(this, SonglistActivity.class);
 		i.putExtra("playlistID", playlistID);
 		startActivity(i);
-		
+
 		// send info to other view, populate other view with items.
 	}
 
@@ -106,11 +112,9 @@ public class PlaylistActivity extends ListActivity {
 		case R.id.action_previous_runs:
 			startActivity(new Intent(this, StatsPage.class));
 			break;
-			/*
-		case R.id.action_songs:
-			// show dialog menu
-			dialog.show();
-			break;*/
+		/*
+		 * case R.id.action_songs: // show dialog menu dialog.show(); break;
+		 */
 		}
 
 		return super.onOptionsItemSelected(item);
--- src/com/example/treadtracksproto/SonglistActivity.java
+++ src/com/example/treadtracksproto/SonglistActivity.java
@@ -2,49 +2,46 @@ package com.example.treadtracksproto;
 
 import java.util.ArrayList;
 
-import android.app.Activity;
 import android.app.ActionBar;
-import android.app.AlertDialog;
 import android.app.Fragment;
 import android.app.ListActivity;
 import android.content.ContentResolver;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
-import android.os.Build;
-import android.provider.MediaStore;
 
 public class SonglistActivity extends ListActivity {
 
 	String playlistID;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_songlist);
-		
+
+		getActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+		getActionBar().setCustomView(R.layout.actionbar);
+
 		Intent intent = getIntent();
 		playlistID = intent.getExtras().getString("playlistID");
-		
+
 		Cursor cursor;
 		ContentResolver cr = this.getContentResolver();
 		String selection = MediaStore.Audio.Media.IS_MUSIC + "!=0";
-		
+
 		ArrayList<SongItem> songData = new ArrayList<SongItem>();
-		
+
 		if (playlistID.equals("all")) {
-			
+
 			// Query the MediaStore for all music files
 			String[] projection = { MediaStore.Audio.Media.TITLE,
 					MediaStore.Audio.Media.ARTIST, MediaStore.Audio.Media.DATA,
@@ -56,9 +53,11 @@ public class SonglistActivity extends ListActivity {
 			cursor.moveToFirst();
 
 			for (int i = 0; i < cursor.getCount(); i++) {
-				songData.add(new SongItem(this, cursor.getString(cursor
-						.getColumnIndex(MediaStore.Audio.Media.TITLE)), cursor
-						.getString(cursor
+				songData.add(new SongItem(
+						this,
+						cursor.getString(cursor
+								.getColumnIndex(MediaStore.Audio.Media.TITLE)),
+						cursor.getString(cursor
 								.getColumnIndex(MediaStore.Audio.Media.ARTIST)),
 						cursor.getString(cursor
 								.getColumnIndex(MediaStore.Audio.Media.DATA)),
@@ -67,22 +66,25 @@ public class SonglistActivity extends ListActivity {
 				cursor.moveToNext();
 			}
 			cursor.close();
-			
+
 		} else {
-			
+
 			String[] projection = { MediaStore.Audio.Playlists.Members.TITLE,
-					MediaStore.Audio.Playlists.Members.ARTIST, MediaStore.Audio.Playlists.Members.DATA,
+					MediaStore.Audio.Playlists.Members.ARTIST,
+					MediaStore.Audio.Playlists.Members.DATA,
 					MediaStore.Audio.Playlists.Members.ALBUM_ID };
 			Long id = Long.parseLong(playlistID);
-			Uri uri = MediaStore.Audio.Playlists.Members.getContentUri("external", id);
+			Uri uri = MediaStore.Audio.Playlists.Members.getContentUri(
+					"external", id);
 			cursor = cr.query(uri, projection, selection, null, null);
 			cursor.moveToFirst();
-			
-			
+
 			for (int i = 0; i < cursor.getCount(); i++) {
-				songData.add(new SongItem(this, cursor.getString(cursor
-						.getColumnIndex(MediaStore.Audio.Playlists.Members.TITLE)), cursor
-						.getString(cursor
+				songData.add(new SongItem(
+						this,
+						cursor.getString(cursor
+								.getColumnIndex(MediaStore.Audio.Playlists.Members.TITLE)),
+						cursor.getString(cursor
 								.getColumnIndex(MediaStore.Audio.Playlists.Members.ARTIST)),
 						cursor.getString(cursor
 								.getColumnIndex(MediaStore.Audio.Playlists.Members.DATA)),
@@ -92,34 +94,37 @@ public class SonglistActivity extends ListActivity {
 			}
 			cursor.close();
 		}
-		
+
 		SongAdapter adapter = new SongAdapter(this, R.layout.song_row_item,
 				songData.toArray(new SongItem[0]));
-		
-		ListView view = getListView(); //(ListView) findViewById(R.id.songlist);
+
+		ListView view = getListView(); // (ListView)
+										// findViewById(R.id.songlist);
 		view.setAdapter(adapter);
-		
-		//view.setOnItemClickListener(listener );
+
+		// view.setOnItemClickListener(listener );
 		/*
-		if (savedInstanceState == null) {
-			getFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}*/
+		 * if (savedInstanceState == null) {
+		 * getFragmentManager().beginTransaction() .add(R.id.container, new
+		 * PlaceholderFragment()).commit(); }
+		 */
 	}
-	
-	protected void onListItemClick(ListView listView, View view, int position, long id) {
+
+	protected void onListItemClick(ListView listView, View view, int position,
+			long id) {
 		super.onListItemClick(listView, view, position, id);
 
-		//SongItem item = (SongItem) listView.getItemAtPosition(position);
-		//Log.d("TAG", "Song name: " + item.getTitle() + " song artist: " + item.getArtist());
-		
-		
+		// SongItem item = (SongItem) listView.getItemAtPosition(position);
+		// Log.d("TAG", "Song name: " + item.getTitle() + " song artist: " +
+		// item.getArtist());
+
 		Intent i = new Intent(this, RunningActivity.class);
 		i.putExtra("playlistID", playlistID);
 		i.putExtra("songPosition", Integer.toString(position));
-		Log.d("TAG", "sending intent playlistID: " + playlistID + " songposition: " + position);
+		Log.d("TAG", "sending intent playlistID: " + playlistID
+				+ " songposition: " + position);
 		startActivity(i);
-		
+
 	}
 
 	@Override
